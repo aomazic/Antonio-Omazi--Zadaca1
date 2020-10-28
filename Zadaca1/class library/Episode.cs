a613bbfeb902402bc5b537f20a47fa00f73eeaa3
aomazic <toniomazc7@gmail.com>
2020-10-28 21:34:41 +0100

    Initial commit. Added info.txt

---
+++ Zadaca1/class library/Episode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class_library
+{
+    class Episode
+    {
+        private int viewers;
+        private double ratingsum;
+        private double maxrating;
+        private double averagerating;
+        public Episode()
+        {
+
+        }
+        public Episode(int Aviewers, double Aratingsum, double Amaxrating)
+        {
+            viewers = Aviewers;
+            ratingsum = Aratingsum;
+            maxrating = Amaxrating;
+        }
+        public void AddView(double score)
+        {
+            viewers++;
+            ratingsum += score;
+            if (score > maxrating) maxrating = score;
+
+        }
+
+        public double GetMaxScore()
+        
+            { return maxrating; }
+
+        
+        public double GetAverageScore()
+        {
+            averagerating = ratingsum / viewers;
+             return averagerating; 
+
+        }
+        public int GetViewerCount()
+        {
+             return viewers; 
+
+        }
+
+
+    }
+}
